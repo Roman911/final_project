303364c61b23f18a19489b3706e1f92bb41b8291
Roman Lysyk <roma-lysyk@ukr.net>
2017-11-26 21:41:19 +0200

    final

---
+++ bakery/src/app.js
@@ -0,0 +1,9 @@
+import slider from './scripts/slider.js';
+import addClass from './scripts/addClass.js';
+
+import './styles.styl';
+
+slider('.slider');
+
+addClass('.nav-bar');
+addClass('.second-page');
